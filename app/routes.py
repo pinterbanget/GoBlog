3b8bda490c16e5d8ee1f00cdcd2d865d14080b73
Ryanson Jonathan <71144324+pinterbanget@users.noreply.github.com>
2025-01-24 00:39:27 +0000

    completed templates tutorial with bootstrap integration

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,32 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Ryan'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'title': 'Beautiful day in Portland!',
+            'body': 'Today was beautiful broskis. I went to the park and saw a squirrel!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'title': 'The Avengers movie',	
+            'body': 'The Avengers movie was so cool! No Way Home still sucks tho #idc'
+        },
+        {
+            'author': {'username': 'Ryan'},
+            'title': 'My first post',
+            'body': 'This is my first post on this website. I hope you all enjoy it!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/hello')
 def hello():
-    return "Hello, who are you?"
+    return "Hello, World!"
 
 @app.route('/user/<username>')
 def show_user_profile(username):
