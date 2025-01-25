9dde13afe5ff4f9d00bb7de3d0c8f83368ab49fb
Ryanson Jonathan <71144324+pinterbanget@users.noreply.github.com>
2025-01-25 00:04:27 +0000

    chapter 4 finished

--- goblog.py
+++ goblog.py
@@ -1,1 +1,8 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post, 'sa': sa, 'orm': orm}
