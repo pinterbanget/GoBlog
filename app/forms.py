e0bde46979d374b1b7af33212219382cdad51dc5
Ryanson Jonathan <71144324+pinterbanget@users.noreply.github.com>
2025-01-26 23:17:07 +0000

    finished chapter 5

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,33 @@
+import sqlalchemy as sa
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
+from app import db
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class SignUpForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_rep = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError('Username already exists!')
+    
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(User.email == email.data))
+        if user is not None:
+            raise ValidationError('Email already exists!')
+        
+    def validate_password(self, password):
+        if len(password.data) < 8:
+            raise ValidationError('Password must be at least 8 characters long!')
--- app/models.py
+++ app/models.py
@@ -1,22 +1,31 @@
 # Model for user database.
 # TODO: change this to just post database
 
-from datetime import datetime, timezone
-from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
-from app import db
+from app import db, login
+from datetime import datetime, timezone
+from typing import Optional
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
     username: orm.Mapped[str] = orm.mapped_column(sa.String(64), index=True, unique=True)
     email: orm.Mapped[str] = orm.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: orm.Mapped[Optional[str]] = orm.mapped_column(sa.String(128))
 
     posts: orm.WriteOnlyMapped['Post'] = orm.relationship(back_populates='author', lazy='dynamic')
+
     def __repr__(self):
         return f'<User {self.username}>'
+    
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
     
 
 class Post(db.Model):
@@ -29,3 +38,8 @@ class Post(db.Model):
     
     def __repr__(self):
         return f'<Post {self.body}>'
+    
+
+@login.user_loader
+def load_user(id: int):
+    return db.session.get(User, int(id))
